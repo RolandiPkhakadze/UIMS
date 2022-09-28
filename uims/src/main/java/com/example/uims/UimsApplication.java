047b8933ff0624eb03185f2e1801b6f0ff16683c
Ioseb Chkhikvadze <iosebi12344@gmail.com>
2022-09-28 14:25:29 +0400

    Initial commit

---
+++ uims/src/main/java/com/example/uims/UimsApplication.java
@@ -0,0 +1,13 @@
+package com.example.uims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UimsApplication.class, args);
+    }
+
+}
---
+++ uims/src/test/java/com/example/uims/UimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UimsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
