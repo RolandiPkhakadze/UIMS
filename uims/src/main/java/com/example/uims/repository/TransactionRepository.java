07f0cadfca57a03deef6f884a65b35fd92de4102
RolandiPkhakadze <rolandi.pkhakadze@gmail.com>
2022-10-04 13:56:43 +0400

    add transaction service

--- uims/src/main/java/com/example/uims/repository/TransactionRepository.java
+++ uims/src/main/java/com/example/uims/repository/TransactionRepository.java
@@ -4,12 +4,13 @@ import com.example.uims.entity.Transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.NonNullApi;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long > {
 
-    @Override
-    boolean existsById( Long id);
-    Optional<Transaction> findTransactionById(Long id);
+    boolean existsById(long id);
+    Optional<Transaction> findTransactionById(long id);
 }
---
+++ uims/src/main/java/com/example/uims/service/TransactionService.java
@@ -0,0 +1,41 @@
+package com.example.uims.service;
+
+import com.example.uims.entity.Transaction;
+import com.example.uims.exception.NotFoundException;
+import com.example.uims.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository repository;
+
+    @Autowired
+    public TransactionService(TransactionRepository repository) {
+        this.repository = repository;
+    }
+
+    public Iterable<Transaction> findAllTransactions() {
+        return repository.findAll();
+    }
+
+    public Transaction findTransactionById(long id) {
+        Optional<Transaction> transactionOptional = repository.findTransactionById(id);
+        checkOptional(transactionOptional, id);
+        return transactionOptional.get();
+    }
+
+    public String getDescription(long id)  {
+        return findTransactionById(id).getDescription();
+    }
+
+    private void checkOptional(Optional<Transaction> optional, long id) {
+        if(optional.isEmpty()){
+            String message = String.format("Transaction with id = %s not found", id);
+            throw new NotFoundException(message);
+        }
+    }
+}
