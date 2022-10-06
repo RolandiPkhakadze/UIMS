573b04a39902a7246a4ad2908cc200163d101452
RolandiPkhakadze <rolandi.pkhakadze@gmail.com>
2022-10-06 16:01:25 +0400

    add user personal number validation

--- uims/src/main/java/com/example/uims/controller/ConvictionController.java
+++ uims/src/main/java/com/example/uims/controller/ConvictionController.java
@@ -1,11 +1,15 @@
 package com.example.uims.controller;
 
 import com.example.uims.entity.Conviction;
+import com.example.uims.entity.User;
 import com.example.uims.service.ConvictionService;
+import com.example.uims.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @Controller
@@ -13,21 +17,40 @@ import org.springframework.web.bind.annotation.*;
 public class ConvictionController {
 
     private final ConvictionService service;
+    private final UserService userService;
+    private String personalNo;
 
     @Autowired
-    public ConvictionController(ConvictionService service) {
+    public ConvictionController(ConvictionService service, UserService userService) {
         this.service = service;
+        this.userService = userService;
     }
 
     @GetMapping("/user/{personalNo}")
     public String getAllConvictionsByUserPersonalNo(@PathVariable(name = "personalNo") final String personalNo, Model model) {
+        model.addAttribute("user", userService.getUserByPersonalNo(personalNo).get());
         model.addAttribute("convictions", service.getAllConvictionsByUserPersonalNo(personalNo));
         return "convictions";
     }
 
+    @ModelAttribute(name = "conviction")
+    public Conviction conviction() {
+        return new Conviction();
+    }
+
+    @GetMapping("/user/{personalNo}/add_new_conviction")
+    public String getConvictionPage(@PathVariable String personalNo, Model model) {
+        this.personalNo = personalNo;
+        return "add_new_conviction";
+    }
+
     @PostMapping
-    public Conviction createConviction(@RequestBody final Conviction conviction) {
-        return service.createConviction(conviction);
+    public String createConviction(@ModelAttribute final Conviction conviction, Model model) {
+        Optional<User> user = userService.getUserByPersonalNo(personalNo);
+        System.out.println(personalNo);
+        conviction.setUser(user.get());
+        service.createConviction(conviction);
+        return "redirect:/conviction/user/" + personalNo;
     }
 
     @GetMapping("/{id}")
